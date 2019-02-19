f2f8452367b40560b1ace51fd7a04ab9e649ed15
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-19 21:07:59 +0100

    simple example

---
+++ test/index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.extentions = require("./index");
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        client.extentions.createMessage(client, {
+            to: msg.channel.id,
+            content: 'Pong!'
+        });
+    }
+});
+
+client.login('token');
