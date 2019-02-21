fec6620329529688cdb8fc185bc737ae33f384ed
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-21 18:13:31 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.extentions = require('../index');
+client.ext = require('../index');
 
 client.on('ready', () => {
+    client.ext.bot.setGame(client, 'I\'m online !');
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
     if (msg.content === 'ping') {
-        client.extentions.createMessage(client, {
+        client.ext.createMessage(client, {
             to: msg.channel.id,
             content: 'Pong!'
         });
