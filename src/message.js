b4257fe9a2079d4e8e8e6fdfc45b8f30f8daaf24
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-22 11:36:22 +0100

    Update message.js

--- src/message.js
+++ src/message.js
@@ -11,24 +11,24 @@ exports.createMessage = (client, options) => {
                 if (!options.content || typeof options.content !== 'string') { 
                     options.content = null;
                 }
-                if (!options.sendEmbed || typeof options.sendEmbed !== 'object') {
-                    options.sendEmbed = null;
+                if (!options.attachEmbed || typeof options.attachEmbed !== 'object') {
+                    options.attachEmbed = null;
                 }
-                if (!options.sendFile || typeof options.sendFile !== 'object') {
-                    options.sendFile = null;
+                if (!options.attachFile || typeof options.attachFile !== 'object') {
+                    options.attachFile = null;
                 }
-                if (options.content === null && options.sendEmbed === null && options.sendFile === null) {
+                if (options.content === null && options.attachEmbed === null && options.attachFile === null) {
                     return console.error('[DISCORD.JS-EXT] You must include a content|embed|file to send a message.');
                 } else {
                     let additional = null;
-                    if (options.sendFile !== null && options.embed !== null) {
-                        additional = Object.assign(options.sendEmbed, options.sendFile);
+                    if (options.attachFile !== null && options.embed !== null) {
+                        additional = Object.assign(options.attachEmbed, options.attachFile);
                     }
-                    else if (options.sendEmbed !== null) {
-                        additional = options.sendEmbed;
+                    else if (options.attachEmbed !== null) {
+                        additional = options.attachEmbed;
                     }
-                    else if (options.sendFile !== null) {
-                        additional = options.sendFile;
+                    else if (options.attachFile !== null) {
+                        additional = options.attachFile;
                     }
                     return client.channels.get(options.to).send(options.content, additional)
                     .catch((err) => {
