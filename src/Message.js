f510bf4409e9a1d483620be1d9f83790f68979d3
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-22 12:34:17 +0100

    Update and rename message.js to Message.js

---
+++ src/Message.js
@@ -0,0 +1,64 @@
+'use strict';
+
+class Message {
+    /**
+     * @constructor
+     * @param {Client} client The bot client
+     */
+    constructor(client) {
+        if (!client) {
+            throw new Error('[DISCORD.JS-EXT] No client found !');
+        }
+        
+        this.client = client;
+    }
+
+    /**
+     * Sends a message from the client
+     * @param {Client} client The bot client
+     * @param {object} options Send options
+     * @returns {Promise<Message>}
+     */
+    create(options) {
+        if (options !== undefined) {
+            if (typeof options === 'object') {
+                if (options.to && !isNaN(options.to)) {
+                    if(!this.client.channels.get(options.to)) {
+                        return console.error('[DISCORD.JS-EXT] The bot cannot send a message in this channel.');
+                    }
+                    if (!options.content || typeof options.content !== 'string') { 
+                        options.content = null;
+                    }
+                    if (!options.attachEmbed || typeof options.attachEmbed !== 'object') {
+                        options.attachEmbed = null;
+                    }
+                    if (!options.attachFile || typeof options.attachFile !== 'object') {
+                        options.attachFile = null;
+                    }
+                    if (options.content === null && options.attachEmbed === null && options.attachFile === null) {
+                        return console.error('[DISCORD.JS-EXT] You must include a content|embed|file to send a message.');
+                    } else {
+                        let additional = null;
+                        if (options.attachFile !== null && options.embed !== null) {
+                            additional = Object.assign(options.attachEmbed, options.attachFile);
+                        }
+                        else if (options.attachEmbed !== null) {
+                            additional = options.attachEmbed;
+                        }
+                        else if (options.attachFile !== null) {
+                            additional = options.attachFile;
+                        }
+                        return this.client.channels.get(options.to).send(options.content, additional)
+                        .catch((err) => {
+                            if (err) {
+                                return console.error(err);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    }
+};
+
+module.exports = Message;
