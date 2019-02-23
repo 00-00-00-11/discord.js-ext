21a716d3e9e1c333b2727ac516d3389b216e3d6d
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-23 18:52:18 +0100

    Update PlayerManager.js

--- src/PlayerManager.js
+++ src/PlayerManager.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ytdl = require('ytdl-core-discord');
+
 class PlayerManager {
     /**
      * @constructor
@@ -29,7 +31,6 @@ class PlayerManager {
                 } else {
                     throw new Error('[DISCORD.JS-EXT] The bot cannot join this channel because it\'s not a voice channel.');
                 }
-                
             } else {
                 throw new Error('[DISCORD.JS-EXT] The bot cannot join this channel because the bot don\'t see it.');
             }
@@ -44,10 +45,43 @@ class PlayerManager {
     async leave(id) {
         if (id && typeof id === 'string') {
             let filterVc = this.client.voiceConnections.filter((v) => v.channel.id === id);
+            let vc = this.client.channels.get(id);
             if (filterVc.size > 0) {
                 await filterVc.first().disconnect();
+            } else if (vc && vc.type === 'voice') {
+                await vc.leave();
             } else {
                 throw new Error('[DISCORD.JS-EXT] The bot cannot leave this channel because the bot don\'t see it.');
+            }
+        }
+    }
+
+    /**
+     * Plays a YouTube video/music
+     * @param {string} id The voice channel ID
+     * @param {string} url The youtube url
+     * @returns {StreamDispatcher}
+     */
+    async playYouTube(id, url) {
+        if (id && typeof id === 'string') {
+            let filterVc = this.client.voiceConnections.filter((v) => v.channel.id === id);
+            let vc = this.client.channels.get(id);
+            if (filterVc.size > 0) {
+                if (url && typeof url === 'string') {
+                    filterVc.first().playOpusStream(await ytdl(url));
+                } else {
+                    throw new Error('[DISCORD.JS-EXT] You must include a valid URL. (string only)');
+                }
+            } else if (vc) {
+                vc.join()
+                .then(async(connection) => {
+                    connection.playOpusStream(await ytdl(url));
+                })
+                .catch((err) => {
+                    if (err) throw new Error(`[DISCORD.JS-EXT] The bot cannot join this channel, an error has occured:\n\n${err.message}`);
+                });
+            } else {
+                throw new Error('[DISCORD.JS-EXT] The bot cannot join this channel because the bot don\'t see it.');
             }
         }
     }
