f1fcd990bc52c523ef314afa9a3f40fbc283f41f
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-24 13:37:19 +0100

    Update PlayerManager.js

--- src/PlayerManager.js
+++ src/PlayerManager.js
@@ -21,69 +21,80 @@ class PlayerManager {
      * @returns {Promise<VoiceConnection>}
      */
     async join(id) {
-        if (id && typeof id === 'string') {
-            if (this.client.channels.get(id)) {
-                if (this.client.channels.get(id).type === 'voice') {
-                    await this.client.channels.get(id).join()
-                    .catch((err) => {
-                        if (err) throw new Error(`[DISCORD.JS-EXT] The bot cannot join this channel, an error has occured:\n\n${err.message}`);
-                    });
+        return new Promise((resolve, reject) => {
+            if (id && typeof id === 'string') {
+                if (this.client.channels.get(id)) {
+                    if (this.client.channels.get(id).type === 'voice') {
+                        this.client.channels.get(id).join()
+                        .then((connection) => {
+                            resolve(connection);
+                        })
+                        .catch(reject);
+                    } else {
+                        reject(new Error('[DISCORD.JS-EXT] The bot cannot join this channel because it\'s not a voice channel.'));
+                    }
                 } else {
-                    throw new Error('[DISCORD.JS-EXT] The bot cannot join this channel because it\'s not a voice channel.');
+                    reject(new Error('[DISCORD.JS-EXT] The bot cannot join this channel because the bot don\'t see it.'));
                 }
-            } else {
-                throw new Error('[DISCORD.JS-EXT] The bot cannot join this channel because the bot don\'t see it.');
             }
-        }
+        });
     }
 
     /**
      * Leave a voice channel
      * @param {string} id The voice channel ID
-     * @returns {void}
+     * @returns {Promise<string>}
      */
     async leave(id) {
-        if (id && typeof id === 'string') {
-            let filterVc = this.client.voiceConnections.filter((v) => v.channel.id === id);
-            let vc = this.client.channels.get(id);
-            if (filterVc.size > 0) {
-                await filterVc.first().disconnect();
-            } else if (vc && vc.type === 'voice') {
-                await vc.leave();
-            } else {
-                throw new Error('[DISCORD.JS-EXT] The bot cannot leave this channel because the bot don\'t see it.');
+        return new Promise(async(resolve, reject) => {
+            if (id && typeof id === 'string') {
+                let filterVc = this.client.voiceConnections.filter((v) => v.channel.id === id);
+                let vc = this.client.channels.get(id);
+                if (filterVc.size > 0) {
+                    await filterVc.first().disconnect();
+                    await resolve("Disconnected!");
+                } else if (vc && vc.type === 'voice') {
+                    await vc.leave();
+                    await resolve("Disconnected!");
+                } else {
+                    reject(new Error('[DISCORD.JS-EXT] The bot cannot leave this channel because the bot don\'t see it.'));
+                }
             }
-        }
+        });
     }
 
     /**
      * Plays a YouTube video/music
      * @param {string} id The voice channel ID
      * @param {string} url The youtube url
-     * @returns {StreamDispatcher}
+     * @returns {Promise<StreamDispatcher>}
      */
     async playYouTube(id, url) {
-        if (id && typeof id === 'string') {
-            let filterVc = this.client.voiceConnections.filter((v) => v.channel.id === id);
-            let vc = this.client.channels.get(id);
-            if (filterVc.size > 0) {
-                if (url && typeof url === 'string') {
-                    filterVc.first().playOpusStream(await ytdl(url), { volume: 0.1 });
+        return new Promise(async(resolve, reject) => {
+            if (id && typeof id === 'string') {
+                let filterVc = this.client.voiceConnections.filter((v) => v.channel.id === id);
+                let vc = this.client.channels.get(id);
+                if (filterVc.size > 0) {
+                    if (url && typeof url === 'string') {
+                        await filterVc.first().playOpusStream(await ytdl(url), { volume: 0.1 });
+                        await resolve(filterVc.first());
+                    } else {
+                        reject(new Error('[DISCORD.JS-EXT] You must include a valid URL. (string only)'));
+                    }
+                } else if (vc) {
+                    vc.join()
+                    .then(async(connection) => {
+                        await connection.playOpusStream(await ytdl(url), { volume: 0.1 });
+                        await resolve(connection);
+                    })
+                    .catch((err) => {
+                        if (err) reject(new Error(`[DISCORD.JS-EXT] The bot cannot join this channel, an error has occured:\n\n${err.message}`));
+                    });
                 } else {
-                    throw new Error('[DISCORD.JS-EXT] You must include a valid URL. (string only)');
+                    reject(new Error('[DISCORD.JS-EXT] The bot cannot join this channel because the bot don\'t see it.'));
                 }
-            } else if (vc) {
-                vc.join()
-                .then(async(connection) => {
-                    connection.playOpusStream(await ytdl(url), { volume: 0.1 });
-                })
-                .catch((err) => {
-                    if (err) throw new Error(`[DISCORD.JS-EXT] The bot cannot join this channel, an error has occured:\n\n${err.message}`);
-                });
-            } else {
-                throw new Error('[DISCORD.JS-EXT] The bot cannot join this channel because the bot don\'t see it.');
             }
-        }
+        });
     }
 };
 
