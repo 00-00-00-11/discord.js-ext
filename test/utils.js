5b49251b09a075ed904377a2536a13f2a2c7b346
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-24 13:56:15 +0100

    Create utils.js

---
+++ test/utils.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const { Client } = require('../index');
+bot.ext = new Client(bot);
+
+bot.on('ready', () => {
+    bot.ext.setGame('I\'m online !');
+    console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.content === 'info') {
+        bot.ext.utils.findMember(msg, msg.author.id).then((res) => {
+            let embed = new Discord.RichEmbed()
+            .setTitle(res.user.tag)
+            .setThumbnail(res.user.displayAvatarURL)
+            .addField('Name', res.user.username)
+            .addField('Tag', res.user.discriminator)
+            .addField('Bot', res.user.bot ? 'Oui' : 'non');
+            bot.ext.message.create({
+                to: msg.channel.id,
+                attachEmbed: embed
+            });
+        });
+    }
+});
+
+bot.login('token');
