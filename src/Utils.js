659294a2e84c8aa47bce074f96792d62ab719481
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-24 13:37:33 +0100

    Update Utils.js

--- src/Utils.js
+++ src/Utils.js
@@ -17,22 +17,22 @@ class Utils {
      * Finds a member
      * @param {Message} message promise of Discord.JS
      * @param {string} search The query
-     * @returns {GuildMember}
+     * @returns {Promise<GuildMember>}
      */
     findMember(message, search) {
         search = search.toLowerCase();
-        return message.guild.members.find((m) => m.displayName.toLowerCase() === search || m.discriminator === search || m.id === search);
+        return Promise.resolve(message.guild.members.find((m) => m.displayName.toLowerCase() === search || m.discriminator === search || m.id === search));
     }
 
     /**
      * Finds a role
      * @param {Message} message promise of Discord.JS
      * @param {string} search The query
-     * @returns {Role}
+     * @returns {Promise<Role>}
      */
     findRole(message, search) {
         search = search.toLowerCase();
-        return message.guild.roles.find((r) => r.name.toLowerCase() === search || r.id === search);
+        return Promise.resolve(message.guild.roles.find((r) => r.name.toLowerCase() === search || r.id === search));
     }
 
     /**
@@ -40,29 +40,31 @@ class Utils {
      * @param {Message} message promise of Discord.JS
      * @param {string} search The query
      * @param {string} type The type of the channel
-     * @returns {TextChannel|VoiceChannel|CategoryChannel}
+     * @returns {Promise<TextChannel|VoiceChannel|CategoryChannel>}
      */
     findChannel(message, search, type) {
         let typeArray = ['text', 'voice', 'category'];
         search = search.toLowerCase();
-        if (!type || type === null) {
-            throw new Error('[DISCORD.JS-EXT] You must include a valid type. (text | voice | category)');
-        }
-        else if (!typeArray.includes(type)) {
-            throw new Error('[DISCORD.JS-EXT] You must include a valid type. (text | voice | category)'); 
-        }
-        return message.guild.channels.filter((c) => c.type === type).find((c) => c.name.toLowerCase() === search || c.id === search);
+        return new Promise((resolve, reject) => {
+            if (!type || type === null) {
+                reject(new Error('[DISCORD.JS-EXT] You must include a valid type. (text | voice | category)'));
+            }
+            else if (!typeArray.includes(type)) {
+                reject(new Error('[DISCORD.JS-EXT] You must include a valid type. (text | voice | category)')); 
+            }
+                resolve(message.guild.channels.filter((c) => c.type === type).find((c) => c.name.toLowerCase() === search || c.id === search));
+        });
     }
     
     /**
      * Finds an emoji
      * @param {Message} message promise of Discord.JS
      * @param {string} search The query
-     * @returns {Emoji}
+     * @returns {Promise<Emoji>}
      */
     findEmoji(message, search) {
         search = search.toLowerCase();
-        return message.guild.emojis.find((e) => e.name.toLowerCase() === search || e.id === search);
+        return Promise.resolve(message.guild.emojis.find((e) => e.name.toLowerCase() === search || e.id === search));
     }
 };
 
