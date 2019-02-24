83890766352547d127f3514c8ed5cc6d66b7e943
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-02-24 13:37:03 +0100

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -19,44 +19,45 @@ class Message {
      * @returns {Promise<Message>}
      */
     create(options = {}) {
-        if (options !== undefined) {
-            if (typeof options === 'object') {
-                if (options.to && !isNaN(options.to)) {
-                    if(!this.client.channels.get(options.to)) {
-                        throw new Error('[DISCORD.JS-EXT] The bot cannot send a message in this channel.');
-                    }
-                    if (!options.content || typeof options.content !== 'string') { 
-                        options.content = null;
-                    }
-                    if (!options.attachEmbed || typeof options.attachEmbed !== 'object') {
-                        options.attachEmbed = null;
-                    }
-                    if (!options.attachFile || typeof options.attachFile !== 'object') {
-                        options.attachFile = null;
-                    }
-                    if (options.content === null && options.attachEmbed === null && options.attachFile === null) {
-                        throw new Error('[DISCORD.JS-EXT] You must include a content|embed|file to send a message.');
-                    } else {
-                        let additional = null;
-                        if (options.attachFile !== null && options.embed !== null) {
-                            additional = Object.assign(options.attachEmbed, options.attachFile);
+        return new Promise((resolve, reject) => {
+            if (options !== undefined) {
+                if (typeof options === 'object') {
+                    if (options.to && !isNaN(options.to)) {
+                        if(!this.client.channels.get(options.to)) {
+                            reject(new Error('[DISCORD.JS-EXT] The bot cannot send a message in this channel.'));
                         }
-                        else if (options.attachEmbed !== null) {
-                            additional = options.attachEmbed;
+                        if (!options.content || typeof options.content !== 'string') { 
+                            options.content = null;
                         }
-                        else if (options.attachFile !== null) {
-                            additional = options.attachFile;
+                        if (!options.attachEmbed || typeof options.attachEmbed !== 'object') {
+                            options.attachEmbed = null;
                         }
-                        return this.client.channels.get(options.to).send(options.content, additional)
-                        .catch((err) => {
-                            if (err) {
-                                throw new Error(err);
+                        if (!options.attachFile || typeof options.attachFile !== 'object') {
+                            options.attachFile = null;
+                        }
+                        if (options.content === null && options.attachEmbed === null && options.attachFile === null) {
+                            reject(new Error('[DISCORD.JS-EXT] You must include a content|embed|file to send a message.'));
+                        } else {
+                            let additional = null;
+                            if (options.attachFile !== null && options.embed !== null) {
+                                additional = Object.assign(options.attachEmbed, options.attachFile);
                             }
-                        });
+                            else if (options.attachEmbed !== null) {
+                                additional = options.attachEmbed;
+                            }
+                            else if (options.attachFile !== null) {
+                                additional = options.attachFile;
+                            }
+                            this.client.channels.get(options.to).send(options.content, additional)
+                            .then((m) => {
+                                resolve(m);
+                            })
+                            .catch(reject);
+                        }
                     }
                 }
             }
-        }
+        });
     }
 };
 
