ae50ef99bfa62ba9eabe9470977d54334aa76459
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-03-29 13:37:12 +0100

    Update PlayerManager.js

--- src/PlayerManager.js
+++ src/PlayerManager.js
@@ -75,7 +75,7 @@ class PlayerManager {
      * Plays a YouTube video/music
      * @param {string} id The voice channel ID
      * @param {string} url The youtube url
-     * @returns {Promise<VoiceConnection>}
+     * @returns {Promise<VoiceBroadcast>}
      */
     playYouTube(id, url) {
         return new Promise(async(resolve, reject) => {
